61e269354d7bbc36e8b3093e0f137b5e501be38b
Austen-gflops2 <154884603+Austen-gflops2@users.noreply.github.com>
2023-12-26 19:24:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,54 @@ import base64
 
 st.title('Chatbot Appearance Customizer')
 
+# Images URLs
+button_bg_color_img = "https://ff81102eb168dd62d43d613bfe6559b8.cdn.bubble.io/f1703589209109x185295492447645300/Bubble%20colour.png"
+icon_color_img = "https://ff81102eb168dd62d43d613bfe6559b8.cdn.bubble.io/f1703589494359x970261616650574800/Icon%20colour.png"
+chat_bg_color_img = "https://ff81102eb168dd62d43d613bfe6559b8.cdn.bubble.io/f1703589213886x642534432192483500/Chat%20Background%20colour.png"
+bot_msg_bg_color_img = "https://ff81102eb168dd62d43d613bfe6559b8.cdn.bubble.io/f1703589203733x368183144715100300/AI%20Text%20colour.png"
+user_msg_bg_color_img = "https://ff81102eb168dd62d43d613bfe6559b8.cdn.bubble.io/f1703589229592x626631308572567200/User%20colour.png"
+text_input_bg_color_img = "https://ff81102eb168dd62d43d613bfe6559b8.cdn.bubble.io/f1703589218413x558643049446593900/Chatbubble%20colour.png"
+
 # Input fields
 chatflowid = st.text_input('Chatflow ID', '40dfbdcb-88cd-4559-aac0-192f302f5811')
 welcome_message = st.text_input('Welcome Message', 'Hello! How can I help you today?')
 
-# Color pickers
-button_bg_color = st.color_picker('Button Background Color', '#3B81F6')
-icon_color = st.color_picker('Icon Color', '#FFFFFF')
-chat_bg_color = st.color_picker('Chat Window Background Color', '#FFFFFF')
-bot_msg_bg_color = st.color_picker('Bot Message Background Color', '#F7F8FF')
-user_msg_bg_color = st.color_picker('User Message Background Color', '#3B81F6')
-text_input_bg_color = st.color_picker('Text Input Background Color', '#FFFFFF')
+# Color pickers with images
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(button_bg_color_img, caption='Button Background Color')
+with col2:
+    button_bg_color = st.color_picker('Button Background Color', '#3B81F6')
+
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(icon_color_img, caption='Icon Color')
+with col2:
+    icon_color = st.color_picker('Icon Color', '#FFFFFF')
+
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(chat_bg_color_img, caption='Chat Window Background Color')
+with col2:
+    chat_bg_color = st.color_picker('Chat Window Background Color', '#FFFFFF')
+
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(bot_msg_bg_color_img, caption='Bot Message Background Color')
+with col2:
+    bot_msg_bg_color = st.color_picker('Bot Message Background Color', '#F7F8FF')
+
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(user_msg_bg_color_img, caption='User Message Background Color')
+with col2:
+    user_msg_bg_color = st.color_picker('User Message Background Color', '#3B81F6')
+
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(text_input_bg_color_img, caption='Text Input Background Color')
+with col2:
+    text_input_bg_color = st.color_picker('Text Input Background Color', '#FFFFFF')
 
 # Sliders
 font_size = st.slider('Font Size', 10, 30, 16)
@@ -43,7 +80,7 @@ chatbot_script = f'''
                     bottom: 20,
                     size: "medium",
                     iconColor: "{icon_color}",
-                    customIconSrc: "https://ff81102eb168dd62d43d613bfe6559b8.cdn.bubble.io/f1703588905402x479216801598927300/4c7e238ebf4f4ea5ed704dffc9340d00.svg?_gl=1*1qj65ui*_gcl_au*ODkxOTM4NjU0LjE3MDE4NTc3NTk.*_ga*ODU4NjMwNDY3LjE3MDE4NTc3NTk.*_ga_BFPVR2DEE2*MTcwMzU2MTAwOC4xNy4xLjE3MDM1ODg3NzQuNDcuMC4w",
+                    customIconSrc: "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg",
                 }},
                 chatWindow: {{
                     welcomeMessage: "{welcome_message}",
